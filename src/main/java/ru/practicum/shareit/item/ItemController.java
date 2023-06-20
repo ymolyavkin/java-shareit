6703b4560e5ec90e4b7910adcc360eceec40caf4
ymolyavkin <mat2017u@yandex.ru>
2023-06-20 09:43:33 +0300

    feat : Added Item class

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.item.model.Item;
 
 /**
  * TODO Sprint add-controllers.
@@ -9,4 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+    public static void main(String[] args) {
+        Item item = new Item.Builder()
+                .id(0L)
+                .name("Name")
+                .description("descr")
+                .available(true)
+                .owner("owner")
+                .request("request")
+                .build();
+        System.out.println();
+    }
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,8 +1,77 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Getter
+@Setter
 public class Item {
+    private long id;
+    @NotBlank(message = "Название вещи не может быть пустым.")
+    private String name;
     private String description;
+    private boolean available;
+    private String owner;
+    private String request;
+
+    public static class Builder {
+        private final Item newItem;
+
+        public Builder() {
+            newItem = new Item();
+        }
+
+        public Builder id(Long id) {
+            newItem.setId(id);
+            return this;
+        }
+
+        public Builder name(String name) {
+            newItem.setName(name);
+            return this;
+        }
+
+        public Builder description(String description) {
+            newItem.setDescription(description);
+            return this;
+        }
+
+        public Builder available(boolean available) {
+            newItem.setAvailable(available);
+            return this;
+        }
+
+        public Builder owner(String owner) {
+            newItem.setOwner(owner);
+            return this;
+        }
+
+        public Builder request(String request) {
+            newItem.setRequest(request);
+            return this;
+        }
+
+        public Item build() {
+            return newItem;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && name.equals(item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
