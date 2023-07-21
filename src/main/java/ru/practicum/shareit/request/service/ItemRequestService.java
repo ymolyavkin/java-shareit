8aa7ab983d533998849df3c36495f1a522c0b647
ymolyavkin <mat2017u@yandex.ru>
2023-07-21 15:18:13 +0500

    fix : fixed one error in ItemRequestController class

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -4,5 +4,5 @@ import ru.practicum.shareit.request.dto.IncomingItemRequestDto;
 import ru.practicum.shareit.user.model.User;
 
 public interface ItemRequestService {
-    User addItemReqest(IncomingItemRequestDto incomingItemRequestDto);
+    User addItemReqest(IncomingItemRequestDto incomingItemRequestDto, Long userId);
 }
