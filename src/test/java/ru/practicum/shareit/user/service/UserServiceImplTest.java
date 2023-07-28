8cc5ee9bd288199ce5545562f671569a405e7172
ymolyavkin <mat2017u@yandex.ru>
2023-07-28 18:39:52 +0300

    test : added getUserById method to UserServiceImplTest class.

---
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceImplTest.java
@@ -0,0 +1,77 @@
+package ru.practicum.shareit.user.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserMapper;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void getUserById_WhenUserFound_thenReturnedUser() {
+        long userId = 1L;
+        User expectUser = new User();
+        expectUser.setId(userId);
+
+        when(userRepository.findById(userId))
+                .thenReturn(Optional.of(expectUser));
+        UserDto userDto = userService.getUserById(userId);
+
+        assertEquals(userDto, UserMapper.mapToUserDto(expectUser));
+    }
+
+    @Test
+    void getUserById_WhenUserNotFound_thenExceptionThrown() {
+        long userId = 1L;
+        when(userRepository.findById(userId))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, ()->userService.getUserById(userId));
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+}
