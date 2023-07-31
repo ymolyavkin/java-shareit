e2f9c7715f84175d7fb4e9941debccdb69512764
ymolyavkin <mat2017u@yandex.ru>
2023-07-31 17:17:25 +0300

    test : added UserRepositoryTest class

---
+++ src/test/java/ru/practicum/shareit/user/repository/UserRepositoryTest.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.user.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByEmailContainingIgnoreCase() {
+    }
+}
