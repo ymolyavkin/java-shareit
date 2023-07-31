f18e11aab234c01383ea10cf6db22bde2bbe71e7
ymolyavkin <mat2017u@yandex.ru>
2023-07-31 17:44:38 +0300

    test : added ItemRequestRepositoryTest class

---
+++ src/test/java/ru/practicum/shareit/request/repository/ItemRequestRepositoryTest.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.request.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+class ItemRequestRepositoryTest {
+@Autowired
+private ItemRequestRepository itemRequestRepository;
+    @Test
+    void findAllByRequesterIdOrderByCreatedDesc() {
+    }
+
+    @Test
+    void findAllByRequesterIdNotOrderByCreatedDesc() {
+    }
+}
