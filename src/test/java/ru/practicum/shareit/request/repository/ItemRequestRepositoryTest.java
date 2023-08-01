9ea735c976d0430afb4ced8e35ad69634305d23f
ymolyavkin <mat2017u@yandex.ru>
2023-08-01 16:53:20 +0500

    style : style

--- src/test/java/ru/practicum/shareit/request/repository/ItemRequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/repository/ItemRequestRepositoryTest.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @DataJpaTest
 class ItemRequestRepositoryTest {
-@Autowired
-private ItemRequestRepository itemRequestRepository;
+    @Autowired
+    private ItemRequestRepository itemRequestRepository;
+
     @Test
     void findAllByRequesterIdOrderByCreatedDesc() {
     }
