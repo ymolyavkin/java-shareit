87084c98fd3b561ab159be108608e3cf4a259f0a
ymolyavkin <mat2017u@yandex.ru>
2023-08-03 15:06:07 +0500

    test : added ItemAnswerToRequestDtoTest

---
+++ src/test/java/ru/practicum/shareit/request/dto/IncomingItemRequestDtoTest.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.request.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IncomingItemRequestDtoTest {
+
+    @Test
+    void testEquals() {
+        String descriptionOne = "Description";
+        String descriptionTwo = "Description";
+
+        assertThat(descriptionOne, equalTo(descriptionTwo));
+    }
+    @Test
+    void testHashCode() {
+        String descriptionOne = "Description";
+        String descriptionTwo = "Description";
+        assertTrue(descriptionTwo.equals(descriptionOne) && descriptionOne.equals(descriptionTwo));
+        assertTrue(descriptionOne.hashCode() == descriptionTwo.hashCode());
+    }
+
+    @Test
+    void getDescription() {
+        String description = "Description";
+
+        IncomingItemRequestDto incomingItemRequestDto = new IncomingItemRequestDto();
+        incomingItemRequestDto.setDescription(description);
+
+        assertEquals(description, incomingItemRequestDto.getDescription());
+    }
+}
