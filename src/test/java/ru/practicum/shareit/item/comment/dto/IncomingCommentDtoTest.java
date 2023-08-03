8fe544345655bd9763fe878a879692410a55dbc1
ymolyavkin <mat2017u@yandex.ru>
2023-08-03 15:46:16 +0500

    test : added IncomingCommentDtoTest

---
+++ src/test/java/ru/practicum/shareit/item/comment/dto/IncomingCommentDtoTest.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.item.comment.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.io.IOException;
+
+@JsonTest
+class IncomingCommentDtoTest {
+    @Autowired
+    private JacksonTester<IncomingCommentDto> json;
+
+    @Test
+    void incomingCommentDtoTest() throws IOException {
+        IncomingCommentDto incomingCommentDto = new IncomingCommentDto();
+        incomingCommentDto.setText("text");
+        JsonContent<IncomingCommentDto> result = json.write(incomingCommentDto);
+
+        Assertions.assertThat(result).extractingJsonPathStringValue("$.text").isEqualTo("text");
+    }
+}
