91eb4a736faff116be1055b6d10176f3b30f80f5
ymolyavkin <mat2017u@yandex.ru>
2023-08-03 15:32:29 +0500

    test : added BookingDtoTest

---
+++ src/test/java/ru/practicum/shareit/booking/dto/BookingMapperTest.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.booking.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingMapperTest {
+
+    @Test
+    void mapToBookingResponseDto() {
+    }
+
+    @Test
+    void mapToBookingDto() {
+    }
+
+    @Test
+    void mapToBookingLastNextDto() {
+    }
+
+    @Test
+    void mapToBooking() {
+    }
+}
