b3d1338ef4c27d3f886032ee0cd85f49441cc310
ymolyavkin <mat2017u@yandex.ru>
2023-08-05 08:20:34 +0300

    test : added addBookingTest()

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -28,8 +28,10 @@ import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
@@ -46,14 +48,14 @@ class BookingServiceImplTest {
     @Mock
     private UserRepository userRepository;
     private final EasyRandom easyRandom = new EasyRandom();
-    private IncomingBookingDto bookingDtoOne;
-    private IncomingBookingDto bookingDtoTwo;
     private IncomingBookingDto incomingBookingDtoOne;
     private IncomingBookingDto incomingBookingDtoTwo;
     private User booker;
+    private User otherBooker;
     private User owner;
     private Item item;
     private Booking booking;
+    private Booking otherBooking;
     private BookingResponseDto bookingResponseDto;
     private int from = 0;
     private int size = 10;
@@ -72,7 +74,7 @@ class BookingServiceImplTest {
         incomingBookingDtoTwo = new IncomingBookingDto();
 
         incomingBookingDtoOne.setBookerId(bookerId);
-        incomingBookingDtoTwo.setBookerId(otherBookerId);
+        incomingBookingDtoTwo.setBookerId(ownerId);
         incomingBookingDtoOne.setItemId(itemId);
         incomingBookingDtoTwo.setItemId(itemId);
 
@@ -101,6 +103,7 @@ class BookingServiceImplTest {
         item = ItemMapper.mapToItem(itemDto, booker);
         item.setId(itemId);
         booking = BookingMapper.mapToBooking(incomingBookingDtoOne, item, booker);
+        otherBooking = BookingMapper.mapToBooking(incomingBookingDtoTwo, item, booker);
         booking.setId(bookingId);
         bookingResponseDto = BookingMapper.mapToBookingResponseDto(booking, item);
     }
@@ -121,6 +124,112 @@ class BookingServiceImplTest {
 
         assertThrows(NotFoundException.class, () -> bookingService.getBookingById(userId, bookingId));
     }
+
+    @Test
+    public void addBookingTest() {
+        long bookerId = 3L;
+        long bookingId = booking.getId();
+        long otherBookingId = owner.getId();
+        incomingBookingDtoOne.setBookerId(bookerId);
+        long itemId = incomingBookingDtoOne.getItemId();
+        // bookingResponseDto userRepository.findById(bookerId)
+
+        Mockito.lenient().when(userRepository.findById(bookerId)).thenReturn(Optional.of(booker));
+        Mockito.lenient().when(itemRepository.findById(item.getId())).thenReturn(Optional.of(item));
+
+        List<Booking> allBookingsByItem = List.of(booking);
+        when(userRepository.findById(otherBookingId)).thenReturn(Optional.of(booker));
+        when(bookingRepository.findByItem_Id(itemId)).thenReturn(allBookingsByItem);
+    //    when(bookingRepository.getReferenceById(anyLong())).thenReturn(booking);
+     //   when(bookingRepository.findById(anyLong())).thenReturn(Optional.ofNullable(booking));
+     //   when(bookingRepository.getReferenceById(otherBookingId)).thenReturn(otherBooking);
+//        when(bookingRepository.save(any()))
+//                .thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+        when(bookingRepository.save(any())).thenReturn(booking);
+        BookingResponseDto bookingdto = bookingService.addBooking(incomingBookingDtoTwo);
+
+        assertThat(bookingdto).hasFieldOrProperty("id");
+    }
+    /*
+    public BookingResponseDto addBooking(IncomingBookingDto incomingBookingDto) {
+        if (incomingBookingDto.getBookerId().equals(-1L)) {
+            throw new NoneXSharerUserIdException("Не указан инициатор бронирования");
+        }
+        Long bookerId = incomingBookingDto.getBookerId();
+        Long itemId = incomingBookingDto.getItemId();
+        User booker = userRepository.findById(bookerId)
+                .orElseThrow(() -> new NotFoundException(String.format("Пользователь с id %d не найден", bookerId)));
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new NotFoundException(String.format("Вещь с id %d не найдена", itemId)));
+        if (bookerId.equals(item.getOwnerId())) {
+            throw new NotFoundException("Инициатор бронирования и владелец запрашиваемой вещи совпадают");
+        }
+        BookingValidation.bookingIsValid(incomingBookingDto, item);
+        Booking resultBooking = BookingMapper.mapToBooking(incomingBookingDto, item, booker);
+        List<Booking> allBookingsByItem = bookingRepository.findByItem_Id(itemId);
+
+        boolean isOverlap = allBookingsByItem
+                .stream()
+                .map(booking -> isOverlapTime(booking, resultBooking))
+                .reduce(Boolean::logicalOr).orElse(false);
+
+        if (isOverlap) {
+            throw new NotFoundException("Данная вещь на этот период недоступна");
+        }
+        Booking booking = bookingRepository.save(resultBooking);
+
+        return BookingMapper.mapToBookingResponseDto(booking, item);
+    }
+     */
+ /*
+    @Test
+    void createBookingsWithFalseAvailableTest() {
+        Long userId = userWithBooking.getId();
+        Long itemId = item.getId();
+        item.setAvailable(false);
+        when(itemRepository.findById(itemId)).thenReturn(Optional.of(item));
+        when(userRepository.findById(any())).thenReturn(Optional.of(userWithBooking));
+        BookingShortDto shortDto = new BookingShortDto(userId, start, end, itemId);
+        assertThrows(NotAvailableException.class, () -> bookingService.createBooking(userId, shortDto));
+
+        verify(bookingRepository, never()).save(any());
+    }
+
+    @Test
+    void createBookingReturnItemNotFoundExceptionTest() {
+        Long userId = userWithBooking.getId();
+        Long itemId = 9999L;
+        when(itemRepository.findById(itemId)).thenReturn(Optional.empty());
+        BookingShortDto shortDto = new BookingShortDto(userId, start, end, itemId);
+
+        assertThrows(NotFoundException.class, () -> bookingService.createBooking(userId, shortDto));
+    }
+
+    @Test
+    void createBookingReturnItemNotAvailableExceptionDateTest() {
+        Long userId = userWithBooking.getId();
+        Long itemId = item.getId();
+        LocalDateTime start = LocalDateTime.now().plusDays(1);
+        LocalDateTime end = LocalDateTime.now().minusDays(2);
+
+        when(itemRepository.findById(itemId)).thenThrow(NotAvailableException.class);
+        BookingShortDto shortDto = new BookingShortDto(userId, start, end, itemId);
+
+        assertThrows(NotAvailableException.class, () -> bookingService.createBooking(userId, shortDto));
+    }
+
+    @Test
+    void createBookingReturnItemNotAvailableExceptionItemIdTest() {
+        Long userId = userWithBooking.getId();
+        Long itemId = 999L;
+
+        when(itemRepository.findById(itemId)).thenThrow(NotAvailableException.class);
+        BookingShortDto shortDto = new BookingShortDto(userId, start, end, itemId);
+
+        assertThrows(NotAvailableException.class, () -> bookingService.createBooking(userId, shortDto));
+    }
+
+     */
 
     @Test
     void getBookingsByOwner() {
