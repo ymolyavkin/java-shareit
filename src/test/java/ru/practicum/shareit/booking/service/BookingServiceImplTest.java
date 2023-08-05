1dd011c3098a8c5ea3d1ccde58549a3b6515bb8a
ymolyavkin <mat2017u@yandex.ru>
2023-08-05 07:01:26 +0300

    test : added approvingBooking_whenAllCorrect_thenApproved() method for BookingServiceImplTest class

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -12,6 +12,7 @@ import ru.practicum.shareit.booking.dto.BookingMapper;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
 import ru.practicum.shareit.booking.dto.IncomingBookingDto;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.exception.NoneXSharerUserIdException;
 import ru.practicum.shareit.exception.NotFoundException;
@@ -180,6 +181,52 @@ class BookingServiceImplTest {
     }
 
     @Test
-    void approvingBooking() {
+    void approvingBooking_whenNotOwner_thenThrown() {
+        long ownerId = 4L;
+        long bookingId = 1L;
+        Throwable thrown = assertThrows(NotFoundException.class, () -> {
+            bookingService.approvingBooking(bookingId, ownerId, true);
+        });
+        assertNotNull(thrown.getMessage());
     }
+
+    @Test
+    void approvingBooking_whenAllCorrect_thenApproved() {
+        long bookingId = 1L;
+        long ownerId = 1L;
+        Mockito.lenient().when(bookingRepository.findById(anyLong())).thenReturn(Optional.ofNullable(booking));
+        Mockito.lenient().when(itemRepository.findById(anyLong())).thenReturn(Optional.of(item));
+        bookingResponseDto.setStatus(Status.APPROVED);
+        BookingResponseDto expected = bookingResponseDto;
+
+        when(bookingRepository.findById(bookingId)).thenReturn(Optional.ofNullable(booking));
+
+        BookingResponseDto actual = bookingService.approvingBooking(bookingId, ownerId, true);
+
+        assertEquals(actual, expected);
+        verify(bookingRepository, times(1)).saveAndFlush(any());
+    }
+    /*
+    public BookingResponseDto approvingBooking(Long bookingId, Long ownerId, Boolean approved) {
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new NotFoundException(String.format("Бронирование с id %d не найдено", bookingId)));
+        Item item = itemRepository.findById(booking.getItemId())
+                .orElseThrow(() -> new NotFoundException(String.format("Вещь с id %d не найдена", booking.getItemId())));
+        if (!ownerId.equals(item.getOwnerId())) {
+            throw new NotFoundException("Подтвержение может быть выполнено только владельцем вещи");
+        }
+        if (approved != null) {
+            if (approved) {
+                if (booking.getStatus() == Status.APPROVED) {
+                    throw new BadRequestException("Status is already approved");
+                }
+                booking.setStatus(Status.APPROVED);
+            } else {
+                booking.setStatus(Status.REJECTED);
+            }
+            bookingRepository.saveAndFlush(booking);
+        }
+        return BookingMapper.mapToBookingResponseDto(booking, item);
+    }
+     */
 }
