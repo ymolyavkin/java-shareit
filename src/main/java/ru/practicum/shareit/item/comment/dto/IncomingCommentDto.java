1890113e71bb5461e7ec2fa80426c252e0dc5fe3
ymolyavkin <mat2017u@yandex.ru>
2023-08-07 16:31:30 +0300

    refactor : removed reference to groups in the @size annotation in the IncomingCommentDto class

--- src/main/java/ru/practicum/shareit/item/comment/dto/IncomingCommentDto.java
+++ src/main/java/ru/practicum/shareit/item/comment/dto/IncomingCommentDto.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.Size;
 @EqualsAndHashCode
 @RequiredArgsConstructor
 public class IncomingCommentDto {
-    @Size(max = 512, groups = {Marker.OnCreate.class, Marker.OnUpdate.class})
+    @Size(max = 512)
     @NotBlank(message = "Комментарий не может быть пустым.")
     private String text;
 }
