5f15b784810270c330aec8c6f13dc389cc5e05fa
ymolyavkin <mat2017u@yandex.ru>
2023-08-14 05:10:06 +0300

    refactor : two handlers

--- gateway/src/main/java/ru/practicum/shareitgateway/error/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareitgateway/error/ErrorHandler.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareitgateway.error;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.exception.ConstraintViolationException;
-import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,7 +10,6 @@ import ru.practicum.shareitgateway.exception.CommentErrorException;
 import ru.practicum.shareitgateway.exception.NoneXSharerUserIdException;
 import ru.practicum.shareitgateway.exception.UnsupportedStatusException;
 
-import javax.persistence.EntityNotFoundException;
 import javax.validation.ValidationException;
 import java.util.Map;
 
@@ -33,32 +30,11 @@ public class ErrorHandler {
         return Map.of("error", e.getMessage());
     }
 
-    @ExceptionHandler({ConstraintViolationException.class})
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleConstraintViolationException(final RuntimeException e) {
-        log.debug("Нарушение ограничения таблицы БД. Статус ошибки 500 INTERNAL_SERVER_ERROR {}", e.getMessage(), e);
-        return Map.of("error", e.getMessage());
-    }
-
     @ExceptionHandler({UnsupportedStatusException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleUnsupportedStatusException(final RuntimeException e) {
         log.debug("Unknown state: UNSUPPORTED_STATUS. Статус ошибки 500 INTERNAL_SERVER_ERROR{}", e.getMessage(), e);
         return Map.of("error", e.getMessage());
-    }
-
-    @ExceptionHandler({JDBCConnectionException.class})
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleJDBCConnectionException(final RuntimeException e) {
-        log.debug("Нарушение ограничений БД. Статус ошибки 500 INTERNAL_SERVER_ERROR{}", e.getMessage(), e);
-        return Map.of("error", "Нарушение ограничений БД. Статус ошибки 500 INTERNAL_SERVER_ERROR");
-    }
-
-    @ExceptionHandler({EntityNotFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleEntityNotFound(final RuntimeException e) {
-        log.debug("Объект не найден в БД. Статус ошибки 404 Not found{}", e.getMessage(), e);
-        return Map.of("error", "Объект не найден в БД. Статус ошибки 404 Not found");
     }
 
     @ExceptionHandler({BadRequestException.class})
